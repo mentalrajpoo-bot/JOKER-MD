e57e7d6cd2039e3aa50170a8991533d3ceebca9b
JOKER-X-LEGEND_MD <mentalrajpoo@gmail.com>
2025-08-07 11:03:35 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,8 +39,8 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
-                newsletterName: config.OWNER_NAME,
+                newsletterJid: '32803556603064129949@newsletter',
+                newsletterName: config.JOKER-MD,
                 serverMessageId: 143
             }
         };
